61b5b295ed49ca3842647828ee9785f2ec1d31e9
Admin <tuandesigner4287@gmail.com>
2024-12-05 16:57:34 +0700

    edit path

--- .build/src/server.js
+++ .build/src/server.js
@@ -1,27 +1,17 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
+const express = require('express');
+const path = require('path');  // Đảm bảo thêm dòng này để sử dụng module path
+const app = express();
 
-// app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular')));
-
-// app.all('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'index.html'));
-// });
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
+// Cấu hình để phục vụ các file tĩnh từ thư mục dist/shopapp-angular/browser
 app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular', 'browser')));
 
 // Đảm bảo mọi route không phải là file tĩnh đều quay lại index.html
 app.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'browser', 'index.html'));
 });
+
+// Lắng nghe trên cổng được Render cung cấp
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
