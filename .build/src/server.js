a3fdae6d63ba8832a36db5b0715fd345af18392a
Admin <tuandesigner4287@gmail.com>
2024-12-05 16:25:43 +0700

    edit server for render

--- .build/src/server.js
+++ .build/src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular', 'browser')));
+app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular')));
 
 app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
