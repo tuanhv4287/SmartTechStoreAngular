4bc4668403f7a9e24b55884c89b4df5632645b43
Admin <tuandesigner4287@gmail.com>
2024-12-05 16:37:20 +0700

    test post

--- .build/src/server.js
+++ .build/src/server.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular')));
+// app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular')));
 
-app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'index.html'));
-});
+// app.all('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'index.html'));
+// });
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+  console.log(`Example app listening on port ${port}`)
+})
