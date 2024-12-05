1d5cbccd6edcbdbbc04c55754bf2117fabaa6536
Admin <tuandesigner4287@gmail.com>
2024-12-05 16:14:39 +0700

    edit dirname

--- .build/src/server.js
+++ .build/src/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist', 'shopapp-angular', 'browser')));
 
 app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'shopapp-angular', 'browser', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
