8c55bfafb4e072a5edc1592b1c9ecbcb17bf77c4
Admin <tuandesigner4287@gmail.com>
2024-12-05 15:56:58 +0700

    edit server

--- .build/src/server.js
+++ .build/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'dist')));
 
+app.all('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
